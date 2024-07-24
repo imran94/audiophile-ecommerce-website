099d3cdd7ea29f68b2fcec5c40d38fb5134bb48f
imran94 <iraja094@gmail.com>
2024-07-24 22:15:16 +0500

    Define Eloquent model relationships

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)->using(CartItem::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function accessories(): HasMany
+    {
+        return $this->hasMany(Accessory::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function carts(): BelongsToMany
+    {
+        return $this->belongsToMany(Cart::class)->using(CartItem::class);
+    }
 }
